f35ef250ea8a22ffa0c77d7c6c6136e2e6cfeb67
JIEEEN <tjtpp009@korea.ac.kr>
2023-01-13 01:51:33 +0900

    [ADD] sequelize, dotenv, db_connection

---
+++ database/models/user.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('user_info', {
+        id:{
+            type: DataTypes.STRING(30),
+            primaryKey: true,
+            unique: true
+        },
+        pwd:{
+            type: DataTypes.STRING(30),
+            unique: false,
+            allowNull: false
+        },
+        name:{
+            type: DataTypes.STRING(30),
+            unique: false,
+            allowNull: false
+        },
+        age:{
+            type: DataTypes.INTEGER,
+            unique: false,
+            allowNull: false
+        },
+        sex:{
+            type: DataTypes.BOOLEAN,
+            unique: false,
+            allowNull: false
+        },
+        loc:{
+            type: DataTypes.STRING(30),
+            unique: false,
+            allowNull: false
+        },
+        phone:{
+            type: DataTypes.STRING(11),
+            unique: false,
+            allowNull: false
+        },
+        email:{
+            type: DataTypes.STRING(30),
+            unique: false,
+            allowNull: true
+        },
+        birth:{
+            type: DataTypes.DATE,
+            unique: false,
+            allowNull: true
+        },
+        nickname:{
+            type: DataTypes.STRING(30),
+            unique: false,
+            allowNull: false
+        }
+    },
+    {
+        timestamps: false
+    }
+    )
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const users = express.Router();
+const { User } = require('../database/models/index');
 
 users
     .route('/') // /user
-    .get((req, res)=>{
-        res.send("User Profile Page _ to be write")
+    .get(async (req, res)=>{
+        const users = await User.findAll();
+        const result = [];
+
+        for(var user of users){
+            result.push({
+                id: user.id,
+                pwd: user.pwd,
+                name: user.name
+            });
+        }
+
+        res.send(result);
     })
     .post((req, res)=>{
         res.send("User Profile Page _ to be write")
